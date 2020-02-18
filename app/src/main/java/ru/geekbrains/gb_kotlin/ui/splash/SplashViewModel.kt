6ce7bfd2a438960d951e00ec8ef3d8e7980120cc
Vitas1968 <dor1968@mail.ru>
2020-02-18 18:22:53 +0700

    подготовили класс для внедрения зависимости

--- app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashViewModel.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashViewModel.kt
@@ -4,10 +4,10 @@ import ru.geekbrains.gb_kotlin.data.NotesRepository
 import ru.geekbrains.gb_kotlin.data.errors.NoAuthException
 import ru.geekbrains.gb_kotlin.ui.base.BaseViewModel
 
-class SplashViewModel : BaseViewModel<Boolean?, SplashViewState>() {
+class SplashViewModel(private val notesRepository: NotesRepository) : BaseViewModel<Boolean?, SplashViewState>() {
 
     fun requestUser() {
-        NotesRepository.getCurrentUser().observeForever {                                                                                                                                                                                                                           //Я копипастил код с урока и не заметил эту надпись
+        notesRepository.getCurrentUser().observeForever {                                                                                                                                                                                                                           //Я копипастил код с урока и не заметил эту надпись
             viewStateLiveData.value = it?.let {
                 SplashViewState(authenticated = true)
             } ?: let {
