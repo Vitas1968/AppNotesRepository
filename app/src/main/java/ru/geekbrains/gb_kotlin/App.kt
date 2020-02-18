99dbfb2f53cb7b6fc6ad9334ace1d16a788a8132
Vitas1968 <dor1968@mail.ru>
2020-02-18 19:00:15 +0700

    запуск Koin

--- app/src/main/java/ru/geekbrains/gb_kotlin/App.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/App.kt
@@ -1,2 +1,15 @@
 package ru.geekbrains.gb_kotlin
 
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+import ru.geekbrains.gb_kotlin.di.appModule
+import ru.geekbrains.gb_kotlin.di.mainModule
+import ru.geekbrains.gb_kotlin.di.noteModule
+import ru.geekbrains.gb_kotlin.di.splashModule
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(appModule, splashModule, mainModule, noteModule))
+    }
+}
