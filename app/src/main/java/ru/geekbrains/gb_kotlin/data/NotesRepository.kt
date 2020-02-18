07cc9144ae111eea43e5513f8b46f07730fe5d26
Vitas1968 <dor1968@mail.ru>
2020-02-18 18:16:40 +0700

    сделали обычным классом с консруктором для внедрения зависимости

--- app/src/main/java/ru/geekbrains/gb_kotlin/data/NotesRepository.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/data/NotesRepository.kt
@@ -7,9 +7,7 @@ import ru.geekbrains.gb_kotlin.data.provider.FireStoreProvider
 import ru.geekbrains.gb_kotlin.data.provider.RemoteDataProvider
 import java.util.*
 
-object NotesRepository {
-
-    private val remoteProvider: RemoteDataProvider = FireStoreProvider()
+class NotesRepository(val remoteProvider: RemoteDataProvider) {
 
     fun getNotes() = remoteProvider.subsrcibeToAllNotes()
     fun saveNote(note: Note) = remoteProvider.saveNote(note)
