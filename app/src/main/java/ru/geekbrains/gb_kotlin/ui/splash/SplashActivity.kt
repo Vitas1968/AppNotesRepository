5e05d97a29f697ecbf1e466e1440fbc8752b64f8
Vitas1968 <dor1968@mail.ru>
2020-02-18 19:22:09 +0700

    внедрение зависимости в свойство

--- app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashActivity.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashActivity.kt
@@ -1,21 +1,20 @@
 package ru.geekbrains.gb_kotlin.ui.splash
 
 import android.os.Handler
-import androidx.lifecycle.ViewModelProvider
+import org.koin.android.viewmodel.ext.android.viewModel
 import ru.geekbrains.gb_kotlin.ui.base.BaseActivity
 import ru.geekbrains.gb_kotlin.ui.main.MainActivity
 
 class SplashActivity : BaseActivity<Boolean?, SplashViewState>() {                                                                                                                                                                                                                                                                                                            //Я копипастил код с урока и не заметил эту надпись
 
-    override val viewModel by lazy {
-        ViewModelProvider(this).get(SplashViewModel::class.java)
-    }
+    override val model: SplashViewModel by viewModel()
+
 
     override val layoutRes: Int? = null
 
     override fun onResume() {
         super.onResume()
-        Handler().postDelayed({ viewModel.requestUser() }, 1000)
+        Handler().postDelayed({ model.requestUser() }, 1000)
     }
 
     override fun renderData(data: Boolean?) {
