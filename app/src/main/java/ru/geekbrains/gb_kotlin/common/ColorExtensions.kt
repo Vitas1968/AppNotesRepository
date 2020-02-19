30f5b26d359e514df558f8c2317e1245be47b879
Vitas1968 <dor1968@mail.ru>
2020-02-19 21:12:10 +0700

    вынесли получение цвета из ресурсов

--- app/src/main/java/ru/geekbrains/gb_kotlin/common/ColorExtensions.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/common/ColorExtensions.kt
@@ -1,2 +1,30 @@
 package ru.geekbrains.gb_kotlin.common
 
+import android.content.Context
+import androidx.core.content.ContextCompat
+import ru.geekbrains.gb_kotlin.R
+import ru.geekbrains.gb_kotlin.data.entity.Note
+
+fun Note.Color.getColorInt(context: Context): Int =
+    ContextCompat.getColor(
+        context, when (this) {
+            Note.Color.WHITE -> R.color.white
+            Note.Color.VIOLET -> R.color.violet
+            Note.Color.YELLOW -> R.color.yellow
+            Note.Color.RED -> R.color.red
+            Note.Color.PINK -> R.color.pink
+            Note.Color.GREEN -> R.color.green
+            Note.Color.BLUE -> R.color.blue
+        }
+    )
+
+
+fun Note.Color.getColorRes(): Int = when (this) {
+    Note.Color.WHITE -> R.color.white
+    Note.Color.VIOLET -> R.color.violet
+    Note.Color.YELLOW -> R.color.yellow
+    Note.Color.RED -> R.color.red
+    Note.Color.PINK -> R.color.pink
+    Note.Color.GREEN -> R.color.green
+    Note.Color.BLUE -> R.color.blue
+}
