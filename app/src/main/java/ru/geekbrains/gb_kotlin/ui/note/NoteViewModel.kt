b47a9ba52e43755bc89845351330d8399d2161c6
Vitas1968 <dor1968@mail.ru>
2020-02-19 18:21:32 +0700

    оптимизируем код для save и delete

--- app/src/main/java/ru/geekbrains/gb_kotlin/ui/note/NoteViewModel.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/ui/note/NoteViewModel.kt
@@ -7,31 +7,25 @@ import ru.geekbrains.gb_kotlin.data.entity.Note
 import ru.geekbrains.gb_kotlin.data.model.NoteResult
 import ru.geekbrains.gb_kotlin.ui.base.BaseViewModel
 
-class NoteViewModel(private val notesRepository: NotesRepository): BaseViewModel<Note?, NoteViewState>() {
-    init {
-        viewStateLiveData.value = NoteViewState()
-    }
+class NoteViewModel(private val notesRepository: NotesRepository): BaseViewModel<NoteViewState.Data, NoteViewState>() {
 
-    private var pendingNote: Note? = null
+
+    private val pendingNote: Note?
+        get() = viewStateLiveData.value?.data?.note
 
     fun save(note: Note) {
-        pendingNote = note
+        viewStateLiveData.value = NoteViewState(NoteViewState.Data(note = note))
     }
 
     fun loadNote(noteId: String) {
-        notesRepository.getNoteById(noteId).observeForever(object : Observer<NoteResult> {
-            override fun onChanged(t: NoteResult?) {
-                t ?: return
-                when (t) {
-                    is NoteResult.Success<*> -> {
-                        viewStateLiveData.value = NoteViewState(note = t.data as Note)
-                    }
-                    is NoteResult.Error -> {
-                        viewStateLiveData.value = NoteViewState(error = t.error)
-                    }
+        notesRepository.getNoteById(noteId).observeForever { result ->
+            result?.let {
+                viewStateLiveData.value = when (result) {
+                    is NoteResult.Success<*> -> NoteViewState(NoteViewState.Data(note = result.data as Note))
+                    is NoteResult.Error -> NoteViewState(error = result.error)
                 }
             }
-        })
+        }
     }
 
     override fun onCleared() {
