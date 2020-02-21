67794171df38496bed87acb6eb6f8157e8ee2398
Vitas1968 <dor1968@mail.ru>
2020-02-21 15:20:31 +0700

    создаем тестовый класс

---
+++ app/src/test/java/ru/geekbrains/gb_kotlin/data/provider/FireStoreProviderTest.kt
@@ -0,0 +1,38 @@
+package ru.geekbrains.gb_kotlin.data.provider
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class FireStoreProviderTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getCurrentUser() {
+    }
+
+    @Test
+    fun subsrcibeToAllNotes() {
+    }
+
+    @Test
+    fun getNoteById() {
+    }
+
+    @Test
+    fun saveNote() {
+    }
+
+    @Test
+    fun deleteNote() {
+    }
+}
