96aa71ff7edb7c49a81f641a46c9289aa4dc2bcc
Vitas1968 <dor1968@mail.ru>
2020-02-21 17:00:29 +0700

    подготовка mockk объектов для теста и сам тест

--- app/src/test/java/ru/geekbrains/gb_kotlin/data/provider/FireStoreProviderTest.kt
+++ app/src/test/java/ru/geekbrains/gb_kotlin/data/provider/FireStoreProviderTest.kt
@@ -1,15 +1,51 @@
 package ru.geekbrains.gb_kotlin.data.provider
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import ru.geekbrains.gb_kotlin.data.entity.Note
+import ru.geekbrains.gb_kotlin.data.errors.NoAuthException
+import ru.geekbrains.gb_kotlin.data.model.NoteResult
+
 
 class FireStoreProviderTest {
+    @get:Rule
+    val taskExecutorRule=InstantTaskExecutorRule()
+
+    private val mockkDb=mockk<FirebaseFirestore>()
+    private val mockkAuth=mockk<FirebaseAuth>()
+    private val mockkResultCollection=mockk<CollectionReference>()
+    private val mockkUser=mockk<FirebaseUser>()
+
+    private val mockkDocument1=mockk<DocumentSnapshot>()
+    private val mockkDocument2=mockk<DocumentSnapshot>()
+    private val mockkDocument3=mockk<DocumentSnapshot>()
+
+    private val testNotes= listOf(Note("1"),Note("2"),Note("3"))
+    private val provider = FireStoreProvider(mockkAuth,mockkDb)
 
     @Before
     fun setUp() {
+        clearAllMocks()
+        every { mockkAuth.currentUser } returns mockkUser
+        every { mockkUser.uid} returns ""
+        every { mockkDb.collection(any()).document(any()).collection(any()) } returns mockkResultCollection
+        every { mockkDocument1.toObject(Note::class.java) } returns testNotes[0]
+        every { mockkDocument2.toObject(Note::class.java) } returns testNotes[1]
+        every { mockkDocument3.toObject(Note::class.java) } returns testNotes[2]
+
     }
 
     @After
@@ -17,22 +53,13 @@ class FireStoreProviderTest {
     }
 
     @Test
-    fun getCurrentUser() {
-    }
+    fun `showld throw NoAuthExeption if no auth`(){
 
-    @Test
-    fun subsrcibeToAllNotes() {
-    }
-
-    @Test
-    fun getNoteById() {
-    }
-
-    @Test
-    fun saveNote() {
-    }
-
-    @Test
-    fun deleteNote() {
+        var result:Any?=null
+        every { mockkAuth.currentUser } returns  null
+        provider.subsrcibeToAllNotes().observeForever {
+            result = (it as? NoteResult.Error)?.error}
+        //assertTrue(result is NoAuthException)
+        assert(result is NoAuthException)
     }
 }
