2a8dfb07021d4791d0bbc6b2a32bac86f1c56808
Vitas1968 <dor1968@mail.ru>
2020-02-22 12:37:40 +0700

    добавляем класс MainViewModelTest

---
+++ app/src/test/java/ru/geekbrains/gb_kotlin/ui/main/MainViewModelTest.kt
@@ -0,0 +1,17 @@
+package ru.geekbrains.gb_kotlin.ui.main
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
