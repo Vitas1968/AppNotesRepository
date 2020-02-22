5a481ae36918e3408b793d5feb106c175c7fe56b
Vitas1968 <dor1968@mail.ru>
2020-02-22 13:28:13 +0700

    тест класс для NoteViewModel

---
+++ app/src/test/java/ru/geekbrains/gb_kotlin/ui/note/NoteViewModelTest.kt
@@ -0,0 +1,17 @@
+package ru.geekbrains.gb_kotlin.ui.note
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class NoteViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
