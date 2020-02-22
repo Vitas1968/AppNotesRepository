3b8ef29f78cade5143ef9d7602cfff507bff8c61
Vitas1968 <dor1968@mail.ru>
2020-02-22 12:42:31 +0700

    необходимая инициализация и тест получения списка заметок

--- app/src/test/java/ru/geekbrains/gb_kotlin/ui/main/MainViewModelTest.kt
+++ app/src/test/java/ru/geekbrains/gb_kotlin/ui/main/MainViewModelTest.kt
@@ -1,17 +1,42 @@
 package ru.geekbrains.gb_kotlin.ui.main
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import ru.geekbrains.gb_kotlin.data.NotesRepository
+import ru.geekbrains.gb_kotlin.data.model.NoteResult
 
 class MainViewModelTest {
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    private val mockRepository = mockk<NotesRepository>()
+    private val notesLiveData = MutableLiveData<NoteResult>()
+
+    private lateinit var viewModel: MainViewModel
 
     @Before
     fun setUp() {
+        clearMocks(mockRepository)
+        every { mockRepository.getNotes() } returns notesLiveData
+        viewModel = MainViewModel(mockRepository)
     }
 
     @After
     fun tearDown() {
+    }
+
+    @Test
+    fun `should call getNotes`() {
+        verify(exactly = 1) { mockRepository.getNotes() }
     }
 }
