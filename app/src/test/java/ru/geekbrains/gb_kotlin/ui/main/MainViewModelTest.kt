f1f095b697859878cc3ee651aa361b7815fcd86f
Vitas1968 <dor1968@mail.ru>
2020-02-22 12:47:53 +0700

    инициализация

--- app/src/test/java/ru/geekbrains/gb_kotlin/ui/main/MainViewModelTest.kt
+++ app/src/test/java/ru/geekbrains/gb_kotlin/ui/main/MainViewModelTest.kt
@@ -1,14 +1,34 @@
 package ru.geekbrains.gb_kotlin.ui.main
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import ru.geekbrains.gb_kotlin.data.NotesRepository
+import ru.geekbrains.gb_kotlin.data.model.NoteResult
 
 class MainViewModelTest {
 
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    private val mockRepository = mockk<NotesRepository>()
+    private val notesLiveData = MutableLiveData<NoteResult>()
+
+    private lateinit var viewModel: MainViewModel
+
+
     @Before
-    fun setUp() {
+    fun setup() {
+        clearMocks(mockRepository)
+        every { mockRepository.getNotes() } returns notesLiveData
+        viewModel = MainViewModel(mockRepository)
     }
 
     @After
