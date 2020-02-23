93e3c4698a18e0c7f730b6defdf3482a09d19b66
Vitas1968 <dor1968@mail.ru>
2020-02-23 10:12:20 +0700

    класс AppTest

---
+++ app/src/androidTest/java/ru/geekbrains/gb_kotlin/AppTest.kt
@@ -0,0 +1,19 @@
+package ru.geekbrains.gb_kotlin
+
+import android.app.Application
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class AppTest : Application() {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
