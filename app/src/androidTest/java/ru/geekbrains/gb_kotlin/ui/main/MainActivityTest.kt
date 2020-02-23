f9813316932f86c354f4936a9253f7a31d838a5f
Vitas1968 <dor1968@mail.ru>
2020-02-23 10:25:13 +0700

    класс MainActivityTest

---
+++ app/src/androidTest/java/ru/geekbrains/gb_kotlin/ui/main/MainActivityTest.kt
@@ -0,0 +1,17 @@
+package ru.geekbrains.gb_kotlin.ui.main
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
