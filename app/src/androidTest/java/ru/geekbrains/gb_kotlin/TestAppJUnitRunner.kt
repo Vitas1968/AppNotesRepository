65748562f993d806f10e1c84fc9ce692b12d223c
Vitas1968 <dor1968@mail.ru>
2020-02-23 10:22:41 +0700

    переопределяем метод newApplication

--- app/src/androidTest/java/ru/geekbrains/gb_kotlin/TestAppJUnitRunner.kt
+++ app/src/androidTest/java/ru/geekbrains/gb_kotlin/TestAppJUnitRunner.kt
@@ -1,4 +1,11 @@
 package ru.geekbrains.gb_kotlin
 
-class TestAppJUnitRunner {
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class TestAppJUnitRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, AppTest::class.java.name, context)
+    }
 }
