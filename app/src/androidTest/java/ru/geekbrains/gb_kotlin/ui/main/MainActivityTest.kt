c4e028c5c40d4631b97038c1fcf246cfa63ef64d
Vitas1968 <dor1968@mail.ru>
2020-02-24 22:46:52 +0700

    MainActivityTest

--- app/src/androidTest/java/ru/geekbrains/gb_kotlin/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/ru/geekbrains/gb_kotlin/ui/main/MainActivityTest.kt
@@ -1,17 +1,65 @@
 package ru.geekbrains.gb_kotlin.ui.main
 
+import androidx.lifecycle.MutableLiveData
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext
+import org.koin.standalone.StandAloneContext.stopKoin
+import ru.geekbrains.gb_kotlin.R
+import ru.geekbrains.gb_kotlin.data.entity.Note
 
 class MainActivityTest {
+    @get:Rule
+    val activityTestRule = IntentsTestRule(MainActivity::class.java, true, false)
+    private val model: MainViewModel = mockk(relaxed = true)
+    private val viewStateLiveData = MutableLiveData<MainViewState>()
+    private val testNotes = listOf(
+        Note("1", "title1", "text1"),
+        Note("2", "title2", "text2"),
+        Note("3", "title3", "text3")
+    )
 
     @Before
     fun setUp() {
+        StandAloneContext.loadKoinModules(
+            listOf(
+                module {
+                    viewModel(override = true) { model }
+                }
+            )
+        )
+        every { model.getViewState() } returns viewStateLiveData
+        activityTestRule.launchActivity(null)
+        viewStateLiveData.postValue(MainViewState(notes = testNotes))
     }
 
     @After
     fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun check_data_is_displayed(){
+        onView(withId(R.id.rv_notes))
+            .perform(scrollToPosition<NotesRVAdapter.ViewHolder>(1))
+        onView(withText(testNotes[1].text))
+            .check(matches(isDisplayed()))
     }
 }
