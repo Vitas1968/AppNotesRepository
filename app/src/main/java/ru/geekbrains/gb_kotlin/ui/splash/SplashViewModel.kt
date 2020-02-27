5a05b8e21e3db757be7a1b8810297d2d9b6fb266
Vitas1968 <dor1968@mail.ru>
2020-02-27 19:34:19 +0700

    починили SplashViewModel

--- app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashViewModel.kt
+++ app/src/main/java/ru/geekbrains/gb_kotlin/ui/splash/SplashViewModel.kt
@@ -1,18 +1,17 @@
 package ru.geekbrains.gb_kotlin.ui.splash
 
+import kotlinx.coroutines.launch
 import ru.geekbrains.gb_kotlin.data.NotesRepository
 import ru.geekbrains.gb_kotlin.data.errors.NoAuthException
 import ru.geekbrains.gb_kotlin.ui.base.BaseViewModel
 
-class SplashViewModel(private val notesRepository: NotesRepository) : BaseViewModel<Boolean?, SplashViewState>() {
+class SplashViewModel(private val notesRepository: NotesRepository) : BaseViewModel<Boolean?>() {
 
     fun requestUser() {
-        notesRepository.getCurrentUser().observeForever {                                                                                                                                                                                                                           //Я копипастил код с урока и не заметил эту надпись
-            viewStateLiveData.value = it?.let {
-                SplashViewState(authenticated = true)
-            } ?: let {
-                SplashViewState(error = NoAuthException())
-            }
+        launch {
+            notesRepository.getCurrentUser()?.let {
+                setData(true)
+            } ?: setError(NoAuthException())
         }
     }
 }
